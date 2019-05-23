de7083914c8d7496abfa8a56b0b9eca73284b604
Peter Juras <peter@orda-app.com>
2019-05-23 10:52:15 +0200

    chore: Update installed packages
    * awsebcli to 3.15.2
    * awscli to 1.16.164
    * serverless cli to 1.43.0

--- generate-dockerfiles.js
+++ generate-dockerfiles.js
@@ -4,11 +4,11 @@ const path = require("path");
 
 const NODE_10_VERSION = "10.15.3";
 const NODE_12_VERSION = "12.2.0";
-const AWS_EB_CLI_VERSION = "3.15.0";
-const AWS_CLI_VERSION = "1.16.149";
+const AWS_EB_CLI_VERSION = "3.15.2";
+const AWS_CLI_VERSION = "1.16.164";
 const NPM_VERSION = "6.9.0";
 const YARN_VERSION = "1.16.0";
-const SERVERLESS_CLI_VERSION = "1.41.1";
+const SERVERLESS_CLI_VERSION = "1.43.0";
 const ANDROID_SDK_VERSION = "28";
 
 const writeFileAsync = promisify(fs.writeFile);
