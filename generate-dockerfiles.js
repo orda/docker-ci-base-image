663710f8ef24181a42477086f5222fdddfd007a4
Peter Juras <peter@orda-app.com>
2019-08-02 09:12:39 +0200

    feat: Update packages
    Node.js 10 to 10.16.1
    awsebcli to 3.15.3
    awscli to 1.16.209
    serverless to 1.49.0

--- generate-dockerfiles.js
+++ generate-dockerfiles.js
@@ -2,13 +2,13 @@ const fs = require("fs");
 const { promisify } = require("util");
 const path = require("path");
 
-const NODE_10_VERSION = "10.16.0";
+const NODE_10_VERSION = "10.16.1";
 const NODE_12_VERSION = "12.7.0";
-const AWS_EB_CLI_VERSION = "3.15.2";
-const AWS_CLI_VERSION = "1.16.207";
+const AWS_EB_CLI_VERSION = "3.15.3";
+const AWS_CLI_VERSION = "1.16.209";
 const NPM_VERSION = "6.10.2";
 const YARN_VERSION = "1.17.3";
-const SERVERLESS_CLI_VERSION = "1.48.4";
+const SERVERLESS_CLI_VERSION = "1.49.0";
 const ANDROID_SDK_VERSION = "28";
 
 const writeFileAsync = promisify(fs.writeFile);
